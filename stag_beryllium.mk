44f36cdb4b17121475feb0719892e60d9cff4ea7
Nolen Johnson <johnsonnolen@gmail.com>
2020-05-03 16:19:27 -0400

    beryllium: Import custom init
    This is a squash of the following commits:
    
      beryllium: Kang pixel5's init to set variant properties
    
        * Based loosely on m8's as of c15efa1ffa8e67469dedc14c779030d3405c0821.
    
        * Lets set fingerprint from polaris oreo miui
    
        * This fixes safetynet ctsProfile
    
      Change-Id: I8bb694340ab02f0bcfb95a09337e14c8fd9017b6
    
      beryllium: Correctly define libinit location
    
        * Adresses: error: 'system/core/init/Android.bp:95:1: "libinit" depends on undefined module "libinit_beryllium"'
    
        Change-Id: Ib663a0dea66abc4c4266989345d057a6237f99fd
    
    Co-authored-by: althafvly <althafvly@gmail.com>
    Change-Id: Iee738279ece845d5d20d3680fa529e471cfd6ec7

--- stag_beryllium.mk
+++ stag_beryllium.mk
@@ -1,16 +1,16 @@
 #
-# Copyright (C) 2018-2021 The LineageOS Project
+# Copyright (C) 2018-2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Syberia stuff.
-$(call inherit-product, vendor/syberia/common.mk)
+# Inherit some common STAG OS stuff.
+$(call inherit-product, vendor/stag/main.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := syberia_beryllium
+PRODUCT_NAME := stag_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
@@ -22,9 +22,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="beryllium-user-10-QKQ1.190828.002-V12.0.3.0.QEJMIXM-release-keys" \
     PRODUCT_NAME="beryllium"
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-#Boot Animation
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_GOOGLE_RECORDER := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi-rev1
