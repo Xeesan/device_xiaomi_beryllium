b1004635fd79411d3181979c97a41bbc526ae62e
Xeesan <ziauddinzisan2001@gmail.com>
2021-12-06 05:26:39 +0600

    CTS Workaround

--- syberia_beryllium.mk
+++ syberia_beryllium.mk
@@ -19,7 +19,7 @@ PRODUCT_MANUFACTURER := Xiaomi
 BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.3.0.QEJMIXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="beryllium-user-10-QKQ1.190828.002-V12.0.3.0.QEJMIXM-release-keys" \
+    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.3.0.QEJMIXM release-keys" \
     PRODUCT_NAME="beryllium"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
