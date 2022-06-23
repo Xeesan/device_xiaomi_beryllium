f5b3a958db206be14274b93d21a3c0ba02fb50c5
Xeesan <ziauddinzisan2001@gmail.com>
2022-06-23 14:36:01 +0600

    beryllium:riceDroid: GO OFFICIAL

--- lineage_beryllium.mk
+++ lineage_beryllium.mk
@@ -6,12 +6,17 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Arcana stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-ARCANA_DEVICE := beryllium
-ARCANA_OFFICIAL := true
-ARCANA_MAINTAINER := 𝕏𝕀𝕊𝔸ℕ
+RICE_DEVICE := beryllium
+RICE_OFFICIAL := true
+RICE_MAINTAINER := 𝕏𝕀𝕊𝔸ℕ
+TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+WITH_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+
 
 # Quick-Tap
 TARGET_SUPPORTS_QUICK_TAP := true
@@ -20,18 +25,16 @@ TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_beryllium
+PRODUCT_NAME := lineage_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
 PRODUCT_MANUFACTURER := Xiaomi
 
-PRODUCT_SYSTEM_NAME := beryllium
-
-BUILD_FINGERPRINT := "google/raven/raven:12/SP2A.220405.004/8233519:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.3.0.QEJMIXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 SP2A.220405.004 8233519 release-keys" \
+    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.3.0.QEJMIXM release-keys" \
     TARGET_PRODUCT="beryllium"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
